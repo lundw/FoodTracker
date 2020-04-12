aaf1d0ac80273104224bb435b7f140ea63b5b063
Will Lund <lundw@cwu.edu>
2020-04-12 06:42:58 -0400

    Fixed photo selector permissions for swift 5

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -41,6 +41,29 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         //Dismiss the picker if the user canceled
         dismiss(animated: true, completion: nil)
     }
+    
+    /*Swift 5 broke this:
+     
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as?
+        UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+     }
+    */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //The info dictionary may contain multiple representations of the image, we want the original
+        guard let selectedImage = info[.originalImage] as?
+        UIImage else{
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        //Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        //Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     // MARK: Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         // Hide the Keyboard
