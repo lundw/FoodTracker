fa364034bfeb813f23f656f21a1f0320f40e1d7b
Will Lund <lundw@cwu.edu>
2020-04-16 06:39:05 -0400

    Corrected new button placement within stack view and added red placeholder color

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,9 +13,32 @@ class RatingControl: UIStackView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        
+        // Create the button
+        
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        
+        //technically already false but good practice
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
     }
 }
