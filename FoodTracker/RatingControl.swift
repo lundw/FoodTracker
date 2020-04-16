458db83c480f1a2b81df41c857a9f78e0c2f594d
Will Lund <lundw@cwu.edu>
2020-04-16 06:19:14 -0400

    Added rating control class and view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Will on 4/15/20.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
